6252436ed68e9f7dab225e22329e39cbe880efa2
Danni-Du <72844387+Danni-Du@users.noreply.github.com>
2025-03-17 23:58:01 -0400

    Update MOM_oda_ml.F90

--- src/ocean_data_assim/MOM_oda_ml.F90
+++ src/ocean_data_assim/MOM_oda_ml.F90
@@ -17,7 +17,7 @@ public :: oda_ml_init, oda_ml_end, oda_ml_inference
 ! Data structure to save the ML configuration, input, and output data
 type, public :: ocean_oda_ml_config ; private
     character(len=255)  :: filename
-    real, dimension(16,66)  :: l1_weight
+    real, dimension(16,61)  :: l1_weight
     real, dimension(16,16)  :: l2_weight, l3_weight
     real, dimension(16) :: l1_bias, l2_bias, l3_bias
     real, dimension(:), allocatable :: z_l
@@ -88,7 +88,7 @@ contains
         real :: PRHO_top, PRHO_bottom, uo_right_top, uo_right_bottom, uo_left_top, uo_left_bottom
         real, dimension(15) :: thetao_zgrad_sigma, so_zgrad_sigma, PRHO_zgrad_sigma, div_sigma, output_DT_sigmas, uo_zgrad_sigma
         real, dimension(:), allocatable :: thetao_zgrad_profile, so_zgrad_profile, div_profile, PRHO_zgrad_profile, uo_zgrad_profile
-        real, dimension(66) :: ANN_input
+        real, dimension(61) :: ANN_input
         real, dimension(:), allocatable :: output_DT_at_zl, output_flux_at_zi
         real, dimension(:), allocatable :: z_l
         real, dimension(16) :: l1_output, l2_output, l3_output
@@ -225,12 +225,12 @@ contains
                     ANN_input(16:30) = PRHO_zgrad_sigma*100
                     ANN_input(31:45) = div_sigma*1E7
                     ANN_input(46) = mld_depth*0.1
-                    ANN_input(47) = tauamp*100
-                    ANN_input(48) = ml_data%latent*0.1
-                    ANN_input(49) = ml_data%sensible*0.1
-                    ANN_input(50) = ml_data%lw*0.1
-                    ANN_input(51) = ml_data%sw*0.1
-                    ANN_input(52:66) = uo_zgrad_sigma*1000
+                    !ANN_input(47) = tauamp*100
+                    !ANN_input(48) = ml_data%latent*0.1
+                    !ANN_input(49) = ml_data%sensible*0.1
+                    !ANN_input(50) = ml_data%lw*0.1
+                    !ANN_input(51) = ml_data%sw*0.1
+                    ANN_input(47:61) = uo_zgrad_sigma*1000
 
                     l1_output = max(ReLU_zero, matmul(ml_config%l1_weight, ANN_input) + ml_config%l1_bias)
                     l2_output = max(ReLU_zero, matmul(ml_config%l2_weight, l1_output) + ml_config%l2_bias)
@@ -322,11 +322,11 @@ contains
         type(ocean_oda_ml_config), pointer, intent(in) :: ml_config
 
         ! character(len=*), intent(in) :: filename
-        ! real, dimension(16,66), intent(out) :: l1_weight
+        ! real, dimension(16,61), intent(out) :: l1_weight
         ! real, dimension(16,16), intent(out) :: l2_weight, l3_weight
         ! real, dimension(16), intent(out) :: l1_bias, l2_bias, l3_bias
 
-        real, dimension(66,16)  :: l1_weight_temp
+        real, dimension(61,16)  :: l1_weight_temp
         real, dimension(16,16) :: l2_weight_temp, l3_weight_temp
         integer :: ncid, varid, retval
         character(len = 255) :: varname
