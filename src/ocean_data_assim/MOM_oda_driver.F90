7cddbad53c0c9477b23c115b6f396e6f2e7b6d3b
Danni-Du <72844387+Danni-Du@users.noreply.github.com>
2025-05-03 19:29:43 -0400

    Update MOM_oda_driver.F90
    just changed the ODA diag field

--- src/ocean_data_assim/MOM_oda_driver.F90
+++ src/ocean_data_assim/MOM_oda_driver.F90
@@ -568,6 +568,22 @@ subroutine init_oda_diags(Time, US, diag_CS, CS)
     endif
   endif
 
+  if (CS%do_T_bias_adjustment .or. CS%do_S_bias_adjustment) then
+    if (CS%do_T_bias_adjustment) then
+      CS%id_inc_t = register_diag_field('ocean_model', 'temp_bc_increment', diag_CS%axesTL, &
+        Time, 'Ocean potential temperature increments predicted by ML', 'degC', conversion=US%C_to_degC)
+      CS%id_inc_t_z = register_diag_field('ocean_model', 'temp_bc_increment_z', diag_CS%axesTZ, &
+      Time, 'Ocean potential temperature increments predicted by ML', 'degC', conversion=US%C_to_degC)
+    endif
+
+    if (CS%do_S_bias_adjustment) then
+      CS%id_inc_s = register_diag_field('ocean_model', 'salt_bc_increment', diag_CS%axesTL, &
+        Time, 'Ocean salinity increments predicted by OTA', 'psu', conversion=US%S_to_ppt)
+      CS%id_inc_s_z = register_diag_field('ocean_model', 'salt_bc_increment_z', diag_CS%axesTZ, &
+        Time, 'Ocean salinity increments predicted by OTA', 'psu', conversion=US%S_to_ppt)
+    endif
+  endif
+
   call cpu_clock_end(id_clock_oda_init)
 
 end subroutine init_oda_diags
