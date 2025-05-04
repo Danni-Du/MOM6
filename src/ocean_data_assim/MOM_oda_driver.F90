b4e3dd413430c61504b26b890d6eac0d6d0229b9
Danni-Du <72844387+Danni-Du@users.noreply.github.com>
2025-05-04 00:13:47 -0400

    Update MOM_oda_driver.F90

--- src/ocean_data_assim/MOM_oda_driver.F90
+++ src/ocean_data_assim/MOM_oda_driver.F90
@@ -853,7 +853,21 @@ subroutine oda(Time, CS)
     
     CS%prior_ave_counter = 0.0
 
-    if (CS%do_T_bias_adjustment .or. CS%do_S_bias_adjustment) call get_bias_correction_tracer(Time, CS%US, CS)
+    if (CS%do_T_bias_adjustment .or. CS%do_S_bias_adjustment) then
+      call get_bias_correction_tracer(Time, CS%US, CS)
+      if (CS%do_T_bias_adjustment) then
+        CS%Ocean_background_ave%T(isc:iec,jsc:jec,:) = CS%Ocean_background_ave%T(isc:iec,jsc:jec,:) + &
+          CS%T_bc_tend(isc:iec,jsc:jec,:) * CS%assim_interval
+        call pass_var(CS%Ocean_background_ave%T, CS%model_G%Domain)
+      endif
+
+      if (CS%do_S_bias_adjustment) then
+        CS%Ocean_background_ave%S(isc:iec,jsc:jec,:) = CS%Ocean_background_ave%S(isc:iec,jsc:jec,:) + &
+          CS%S_bc_tend(isc:iec,jsc:jec,:) * CS%assim_interval
+        call pass_var(CS%Ocean_background_ave%S, CS%model_G%Domain)
+      endif
+
+    endif
 
     if (CS%do_T_ml_bias_adjustment .or. CS%do_S_ml_bias_adjustment) then
 
