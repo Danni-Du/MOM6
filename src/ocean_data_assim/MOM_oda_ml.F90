814f57df3d042802fa7f99a5e031fa229d2b4b7a
Danni-Du <72844387+Danni-Du@users.noreply.github.com>
2025-05-19 01:43:57 -0400

    Update MOM_oda_ml.F90

--- src/ocean_data_assim/MOM_oda_ml.F90
+++ src/ocean_data_assim/MOM_oda_ml.F90
@@ -302,7 +302,7 @@ contains
 
                     ANN_input(53) = (log10(thetao_zgrad_sigma_dist+1E-3)+0.78)/0.48
                     ANN_input(54) = (log10(PRHO_zgrad_sigma_dist+1E-3)+1.32)/0.52
-                    ANN_input(55) = (log10(shear2_sigma_dist+1E-8)+4.18)/0.86
+                    ANN_input(55) = (log10(shear2_sigma_dist+1E-8)+4.17)/0.86
 
                     ANN_input_final(1:7) = ANN_input(31:37)
                     ANN_input_final(8:10) = ANN_input(53:55)
