a5c5bd3e0f8e0421f814809ef898bcb0ced352d7
Danni-Du <72844387+Danni-Du@users.noreply.github.com>
2025-05-19 13:34:35 -0400

    Update MOM_oda_ml.F90

--- src/ocean_data_assim/MOM_oda_ml.F90
+++ src/ocean_data_assim/MOM_oda_ml.F90
@@ -215,11 +215,11 @@ contains
             if (zl_index_3mld == 0) then ! if 3 mld not found
                 ml_data%T_inc=0.0
             else
-                if (z_l(zl_index_3mld+1) > ml_data%bathyT .OR. &
-                    z_l(zl_index_3mld+1) > ml_data%bathyU_left .OR. &
-                    z_l(zl_index_3mld+1) > ml_data%bathyU_right .OR. &
-                    z_l(zl_index_3mld+1) > ml_data%bathyV_south .OR. &
-                    z_l(zl_index_3mld+1) > ml_data%bathyV_north) then
+                if (ml_config%z_i(zl_index_3mld+2) > ml_data%bathyT .OR. &
+                    ml_config%z_i(zl_index_3mld+2) > ml_data%bathyU_left .OR. &
+                    ml_config%z_i(zl_index_3mld+2) > ml_data%bathyU_right .OR. &
+                    ml_config%z_i(zl_index_3mld+2) > ml_data%bathyV_south .OR. &
+                    ml_config%z_i(zl_index_3mld+2) > ml_data%bathyV_north) then
                     ml_data%T_inc=0.0
                 else ! if above all bathy, then get the vertical profiles
 
